fbccaad26edfb1aa6198061e0fd01914c718c87a
“Matt <matt.s.demartino@gmail.com>
2022-08-24 15:32:58 -0400

    Refactor Script

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,30 +1,36 @@
-document.addEventListener("keydown", function (e) {
-  if (e.key === "a") {
-    let audio = new Audio("sounds/clap.wav");
-    audio.play();
-  } else if (e.key === "s") {
-    let audio = new Audio("sounds/hihat.wav");
-    audio.play();
-  } else if (e.key === "d") {
-    let audio = new Audio("sounds/kick.wav");
-    audio.play();
-  } else if (e.key === "f") {
-    let audio = new Audio("sounds/openhat.wav");
-    audio.play();
-  } else if (e.key === "g") {
-    let audio = new Audio("sounds/boom.wav");
-    audio.play();
-  } else if (e.key === "h") {
-    let audio = new Audio("sounds/ride.wav");
-    audio.play();
-  } else if (e.key === "j") {
-    let audio = new Audio("sounds/snare.wav");
-    audio.play();
-  } else if (e.key === "k") {
-    let audio = new Audio("sounds/tom.wav");
-    audio.play();
-  } else if (e.key === "l") {
-    let audio = new Audio("sounds/tink.wav");
-    audio.play();
-  }
-});
+// window.addEventListener("keydown", function (e) {
+//   if (e.key === "a") {
+//     let audio = new Audio("sounds/clap.wav");
+//     audio.play();
+//   } else if (e.key === "s") {
+//     let audio = new Audio("sounds/hihat.wav");
+//     audio.play();
+//   } else if (e.key === "d") {
+//     let audio = new Audio("sounds/kick.wav");
+//     audio.play();
+//   } else if (e.key === "f") {
+//     let audio = new Audio("sounds/openhat.wav");
+//     audio.play();
+//   } else if (e.key === "g") {
+//     let audio = new Audio("sounds/boom.wav");
+//     audio.play();
+//   } else if (e.key === "h") {
+//     let audio = new Audio("sounds/ride.wav");
+//     audio.play();
+//   } else if (e.key === "j") {
+//     let audio = new Audio("sounds/snare.wav");
+//     audio.play();
+//   } else if (e.key === "k") {
+//     let audio = new Audio("sounds/tom.wav");
+//     audio.play();
+//   } else if (e.key === "l") {
+//     let audio = new Audio("sounds/tink.wav");
+//     audio.play();
+//   }
+// });
+
+const playSound = (e) => {
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  audio.play();
+};
+window.addEventListener("keydown", playSound);
